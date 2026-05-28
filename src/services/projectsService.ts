3a3a3a8b7124bacee485d0387357a4e97a7ead0f
HashimMuhaned <hashimcode123@gmail.com>
2026-05-28 10:33:04 +0400

    replaing fetch link from local to prod

--- src/services/projectsService.ts
+++ src/services/projectsService.ts
@@ -43,8 +43,8 @@ export interface Project {
   createdAt?: number | string;
 }
 
-// const API_URL = "https://echo-shift-tech-backend.vercel.app/api/projects";
-const API_URL = "http://localhost:5000/api/projects";
+const API_URL = "https://echo-shift-tech-backend.vercel.app/api/projects";
+// const API_URL = "http://localhost:5000/api/projects";
 
 function getToken() {
   if (typeof window === "undefined") return null;
